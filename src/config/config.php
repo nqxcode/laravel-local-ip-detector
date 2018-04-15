6899871ade101d139f6f32a5a05fa0520df8f9df
nqxcode <nc101ux@gmail.com>
2018-04-15 16:33:41 +0300

    Fix comment in config.php

--- src/config/config.php
+++ src/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'client_ips' => ['127.0.0.1',], // store cookie with local ip for client ips
+    'client_ips' => ['127.0.0.1',], // client ips for which local ip will be stored to cookie
     'cookie' => [
         'key' => 'local-ip', // name of cookie with local ip address
         'expires' => 24 * 3600, // expires in seconds, 1 day by default
